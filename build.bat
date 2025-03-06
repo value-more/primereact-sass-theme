a7b1ea670a946eb5ae704f1d25f2a35e57d57924
Quentin Vanhauteghem <quentvht@hotmail.com>
2025-03-07 00:24:31 +0100

    first version of value-more-light

--- build.bat
+++ build.bat
@@ -8,9 +8,4 @@ call npm run sass
 mkdir %RESOURCES%\value-more-light
 mkdir %RESOURCES%\value-more-light\fonts
 copy build\themes\value-more\value-more-light\theme.css %RESOURCES%\value-more-light\theme.css
-copy themes\value-more\value-more-light\fonts %RESOURCES%\value-more-light\fonts\*
-
-mkdir %RESOURCES%\value-more-dark
-mkdir %RESOURCES%\value-more-dark\fonts
-copy build\themes\value-more\value-more-dark\theme.css %RESOURCES%\value-more-dark\theme.css
-copy themes\value-more\value-more-dark\fonts %RESOURCES%\value-more-dark\fonts\*
+copy themes\value-more\fonts %RESOURCES%\value-more-light\fonts\*
