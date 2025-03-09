791feb31b74e07f7369a8c972caa85d9f8695607
Quentin Vanhauteghem <quentvht@hotmail.com>
2025-03-09 03:45:16 +0100

    set background var for mira theme

--- build.bat
+++ build.bat
@@ -9,3 +9,5 @@ mkdir %RESOURCES%\value-more-light
 mkdir %RESOURCES%\value-more-light\fonts
 copy build\themes\value-more\value-more-light\theme.css %RESOURCES%\value-more-light\theme.css
 copy themes\value-more\fonts %RESOURCES%\value-more-light\fonts\*
+
+copy build\themes\mira\theme.css %RESOURCES%\mira\theme.css
